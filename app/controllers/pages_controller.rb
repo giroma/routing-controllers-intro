6b714ec9626ad43db1fce9d3891b3f1999289786
Argir Argirov <dj.giro@gmail.com>
2017-10-11 15:43:40 -0400

    dynamic cat routes added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   def welcome
-    render :about
+    @header = "This is the welcome page"
+  end
+  def about
+
+  end
+  def contest
+
+  end
+  def kitten
+    @kitten_url = "http://lorempixel.com/#{params[:size]}/#{params[:size]}/cats"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
   get 'welcome' => 'pages#welcome'
+  get 'about' => 'pages#about'
+  get 'contest' => 'pages#contest'
+  get 'kitten/:size' => 'pages#kitten'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
